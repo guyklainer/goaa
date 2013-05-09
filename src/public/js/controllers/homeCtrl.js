8395be7f507ac3753944deeb01e36c1f98651a8e
Oryanbit <oryan.mishali@gmail.com>
2013-05-09 18:24:29 +0300

    groups home screen

--- src/public/js/controllers/homeCtrl.js
+++ src/public/js/controllers/homeCtrl.js
@@ -2,5 +2,28 @@
 angular.module('App').controller('HomeCtrl', ['$scope', 'blockui', '$http', '$location', 'account',
     function($scope, blockui, $http, $location, account){
 
+    var groups =
+        [new GroupViewModel("group1"),
+        new GroupViewModel("group2"),
+        new GroupViewModel("group3")]
+        ;
+
+    // public var
+    $scope.groups = groups;
+
+
+
     $scope.account = account;
 }]);
+
+var GroupViewModel = function(groupName){
+    return {
+        name: groupName,
+        address: {
+            street: "",
+            city: "",
+            number: 0
+        },
+        imgLocation: ""
+    }
+};
