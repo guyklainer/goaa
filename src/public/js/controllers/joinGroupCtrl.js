aa53e130c7cd6cd7cf4ce3d91d37fb51d6e07184
Guy Klainer <guyklainer@gmail.com>
2013-05-17 18:57:22 +0300

    Make search group nicer

--- src/public/js/controllers/joinGroupCtrl.js
+++ src/public/js/controllers/joinGroupCtrl.js
@@ -3,12 +3,28 @@ angular.module('App').controller('JoinGroupCtrl', ['$scope', 'blockui', '$http',
     function($scope, blockui, $http, $location, account, $cookies, db){
 
     // public var
-    $scope.query = "";
-    $scope.groups = [];
+    $scope.query        = "";
+    $scope.groups       = [];
+    $scope.timer        = null;
+    $scope.showLoader   = false;
 
 
     //public functions
+
     $scope.search = function(){
+        $scope.showLoader = true;
+
+        if( $scope.timer != null ) {
+            clearTimeout( $scope.timer );
+            $scope.timer = null;
+        }
+
+        $scope.timer = setTimeout( function(){
+            makeSearch();
+        }, 1000 );
+    }
+
+    var makeSearch = function(){
         log("search for: " + $scope.query);
 
         db.add('groupsSearchQuery', $scope.query);
@@ -16,16 +32,21 @@ angular.module('App').controller('JoinGroupCtrl', ['$scope', 'blockui', '$http',
         $http.post('/searchgroups', { groupName: $scope.query })
             .error(function(data, status, headers, config){
                 httpErrorCallback(data, status, headers, config);
+                $scope.showLoader = false;
             })
             .success(function(data, status, headers, config) {
                 log(data);
+
                 if (data.result && angular.isArray(data.data)){
                     $scope.groups = data.data;
                     db.add('groupsSearch', $scope.groups);
+
                 } else {
                     $scope.groups = [];
                     db.add('groupsSearch', $scope.groups);
                 }
+
+                $scope.showLoader = false;
             });
     };
 
