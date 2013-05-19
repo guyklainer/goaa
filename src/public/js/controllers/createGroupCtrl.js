83fedd0d9dd02fd7bedff5b0f331407e5ca309d5
Oryanbit <oryan.mishali@gmail.com>
2013-05-19 23:24:18 +0300

    upload with out submit

--- src/public/js/controllers/createGroupCtrl.js
+++ src/public/js/controllers/createGroupCtrl.js
@@ -2,8 +2,13 @@
 angular.module('App').controller('CreateGroupCtrl', ['$scope', 'blockui', '$http', '$location','account',
     function($scope, blockui, $http, $location, account){
 
+        $("#fileChooser").change(function (ev) {
+            fileChooserOnChange();
+        });
+
         // public var
         $scope.isShowError = false;
+        $scope.photoErrorMsg = "";
         $scope.errorInDataBaseSaving = false;
         $scope.Group = {
             name     :"",
@@ -15,54 +20,88 @@ angular.module('App').controller('CreateGroupCtrl', ['$scope', 'blockui', '$http
             image    : ""
         };
 
-        $("#fileChooser").change(function (ev) {
-            fileChooserOnChange(ev, this);
-        });
-        var fileChooserOnChange = function(ev, _this) {
-            log("file chooser on change");
-
-            showImage(ev, _this);
-
-            var isValid = false;
-            var fileName = $("#photoImg").data("name");
-
-            log("filename: ");
-            log(fileName);
-
-            if (fileName != undefined) { //case there is a photo
-
-                // validate file extention is correct
-                var extension = fileName.substr((fileName.lastIndexOf('.') + 1));
-
-                log("extension");
-                log(extension);
-
-                switch (extension) {
-                    case 'jpeg':
-                    case 'jpg':
-                    case 'png':
-                    case 'gif':
-                        isValid = true;
-                        break;
-                }
-
-                if (isValid) {
-                    $("#uploadImageForm").submit();
-
-                } else {
-                    //todo show error please choose photo
+        var uploadComplete = function(res) {
+//            alert("complete - " + res.currentTarget.responseText);
+            log("uploadComplete");
+            var jsonResponse = angular.fromJson(res.currentTarget.responseText);
+            log(jsonResponse);
+            if (jsonResponse.result){
+                showImage();
+                //$scope.Group.image = json.image;
+            } else {
+                if (jsonResponse.msg.toLowerCase() == "toobig"){
+                    $scope.photoErrorMsg = "The photo is too big";
                 }
             }
         },
-        showImage = function(ev, _this){
+        uploadFailed = function(res) {
+            //log("uploadFailed");
+            //log(res);
+            alert("failed");
+            //todo: show could not upload
+        },
+        uploadCanceled = function(res) {
+            alert("canceled");
+            //log("uploadCanceled");
+            //log(res);
+        },
+        uploadProgress =function(res) {
+            log("uploadProgress");
+            log(res);
+        },
+        fileChooserOnChange = function() {
+            log("file chooser on change");
+
+            var fileChooser = document.getElementById('fileChooser');
+            var fileType = fileChooser.files[0].type;
+
+            if (isFileValid(fileType)) {
+                //sending the image
+                uploadImage();
+
+            } else {
+                //todo: show please choose photo
+            }
+
+        },
+        isFileValid = function(fileType){
+            var isValid = false;
+
+            if (fileType != undefined) {
+                var subType = fileType.substr(0,fileType.lastIndexOf('/'));
+                if (subType == "image"){
+                    isValid = true;
+                }
+            }
+
+            log("is file valid: " + isValid);
+
+            return isValid;
+        },
+        showImage = function(){
+            var fileChooser = document.getElementById('fileChooser');
+            var file = fileChooser.files[0];
+
             var reader = new FileReader();
             reader.onload = (function (ev) {
-                $("#photoImg").attr("src", ev.target.result).fadeIn();
+                $("#photoImg").attr("src", ev.target.result).slideDown();
             });
 
-            var file = _this.files[0];
             $("#photoImg").data("name", file.name);
             reader.readAsDataURL(file);
+        },
+        uploadImage = function() {
+            var fileChooser = document.getElementById('fileChooser');
+            var fd = new FormData();
+            fd.append("image", fileChooser.files[0]);
+
+            var xhr = new XMLHttpRequest();
+            xhr.upload.addEventListener("progress", uploadProgress, false);
+            xhr.addEventListener("load", uploadComplete, false);
+            xhr.addEventListener("error", uploadFailed, false);
+            xhr.addEventListener("abort", uploadCanceled, false);
+            xhr.open("POST", "/api/upload");
+            xhr.send(fd)
         };
 
         // public functions
@@ -106,11 +145,8 @@ angular.module('App').controller('CreateGroupCtrl', ['$scope', 'blockui', '$http
                     }
                 });
         };
-        $scope.addPhoto = function (group){
-            log("sending to upload image");
-
-//            blockui.block();
-              $("#fileChooser").click();
+        $scope.addPhoto = function(){
+            $("#fileChooser").click();
         };
 
         $scope.account = account;
