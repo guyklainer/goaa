71627d1049428f2c746dfc2c13fdec7d96e17a81
Oryanbit <oryan.mishali@gmail.com>
2013-05-26 16:00:30 +0300

    group view

--- src/public/js/controllers/groupCtrl.js
+++ src/public/js/controllers/groupCtrl.js
@@ -1,27 +1,17 @@
 "use strict";
 
-angular.module('App').controller('GroupCtrl', ['$scope', 'blockui', '$http', '$location', 'account', '$routeParams','$timeout',
-    function($scope, blockui, $http, $location, account, $routeParams, $timeout){
+angular.module('App').controller('GroupCtrl', ['$scope', 'blockui', '$http', '$location', 'account', '$routeParams','$timeout', 'groupDb',
+    function($scope, blockui, $http, $location, account, $routeParams, $timeout, groupDb){
 
-        log("group Id: ", $routeParams.id);
+        log("group Id: ", $routeParams.name);
+        log("group view: ", $routeParams.view);
 
-        $scope.group = {
-            name: "temp group",
-            address  : {
-                country: "",
-                street: "",
-                city: "",
-                house: "",
-                apartment: ""
-            },
-            image: "",
-            posts: [
-                new PostModel(),
-                new PostPhotoModel(),
-                new PostModel(),
-                new PostModel()
-            ]
-        };
+        $scope.view = $routeParams.view;
+
+        groupDb.getGroup($routeParams.name, function(g){
+            log("callback res: ", g);
+            $scope.group = g;
+        });
 
         var len = 200;
         $scope.toShortStr = function(post){
@@ -33,11 +23,9 @@ angular.module('App').controller('GroupCtrl', ['$scope', 'blockui', '$http', '$l
 
             return str;
         }
-
         $scope.toggleTextLength = function(post){
             post.isShowFull = !post.isShowFull;
         }
-
         $scope.isTooBigMessage = function(message){
             var result = false;
 
@@ -47,26 +35,15 @@ angular.module('App').controller('GroupCtrl', ['$scope', 'blockui', '$http', '$l
 
             return result;
         }
+        $scope.gotoGallery = function(){
+            if ($scope.view == undefined || $scope.view == null){
+                $location.path( $location.path() + "/gallery" );
+            } else if ($scope.view.toLowerCase() != 'gallery'){
+                log("path: ", $location.path());
+            }
+        }
 
         $scope.account = account;
     }
 ]);
 
-function PostModel(){
-    return {
-        id: "",
-        username: "Oryan Mishali",
-        date: new Date().setDate(-1),
-        message: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus cursus, magna id porta aliquet, enim nisi consequat risus, non varius lacus lorem vel felis. Nunc posuere adipiscing feugiat. Praesent hendrerit suscipit metus, quis accumsan libero imperdiet nec. Vivamus molestie turpis ac nunc eleifend et auctor nunc viverra. Pellentesque metus nisi, malesuada in elementum vitae, pulvinar bibendum nisl. Ut felis mi, blandit sed blandit at, lacinia sed nulla. Curabitur sollicitudin arcu a leo gravida ultricies. Donec massa purus, congue non elementum ac, volutpat ac lacus. Integer molestie turpis condimentum neque lobortis convallis. Nam nisl lorem, dapibus at pretium scelerisque, dignissim eu neque. Vestibulum quis nisl sem, eu porttitor nibh. In vulputate ipsum quis tortor sagittis eu ultricies nunc imperdiet. Nam eros enim, euismod quis ultricies sit amet, sagittis eget nisi. Suspendisse feugiat commodo neque ut fringilla. Nam odio erat, ultrices aliquet imperdiet vitae, luctus vitae purus.",
-        photoUrl: ""
-    }
-};
-function PostPhotoModel(){
-    return {
-        id: "",
-        username: "Shushana",
-        date: new Date().setDate(-1),
-        message: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus cursus, magna id porta aliquet, enim nisi consequat risus, non varius lacus lorem vel felis. Nunc posuere adipiscing feugiat. Praesent hendrerit suscipit metus, quis accumsan libero imperdiet nec. Vivamus molestie turpis ac nunc eleifend et auctor nunc viverra. Pellentesque metus nisi, malesuada in elementum vitae, pulvinar bibendum nisl. Ut felis mi, blandit sed blandit at, lacinia sed nulla. Curabitur sollicitudin arcu a leo gravida ultricies. Donec massa purus, congue non elementum ac, volutpat ac lacus. Integer molestie turpis condimentum neque lobortis convallis. Nam nisl lorem, dapibus at pretium scelerisque, dignissim eu neque. Vestibulum quis nisl sem, eu porttitor nibh. In vulputate ipsum quis tortor sagittis eu ultricies nunc imperdiet. Nam eros enim, euismod quis ultricies sit amet, sagittis eget nisi. Suspendisse feugiat commodo neque ut fringilla. Nam odio erat, ultrices aliquet imperdiet vitae, luctus vitae purus.",
-        photoUrl: "http://www.gravatar.com/avatar/d41d8cd98f00b204e9800998ecf8427e.jpg?s=192"
-    }
-};
