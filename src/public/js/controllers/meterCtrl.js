67741d384c07c99c7fd4ecbe276cf35404ae110c
Guy Klainer <guyklainer@gmail.com>
2013-05-28 01:16:40 +0300

    design meter page

--- src/public/js/controllers/meterCtrl.js
+++ src/public/js/controllers/meterCtrl.js
@@ -6,20 +6,63 @@ angular.module('App').controller('MeterCtrl', ['$scope', 'blockui', '$http', '$l
         log($routeParams.groupName);
         log($routeParams.meter);
 
-        $scope.groupName = $routeParams.groupName;
-        $scope.meterName = $routeParams.meter;
-        $scope.temperature = 20;
+        $scope.showBottomMenu   = true;
+        $scope.groupName        = $routeParams.groupName;
+        $scope.meterName        = $routeParams.meter;
+        $scope.boiler           = {
+            status  :   'off',
+            data    :   20
+        }
+
+//        var socket = io.connect();
+//
+//        socket.on( 'boiler', function( boiler ){
+//            $scope.boiler = boiler;
+//
+//            $('.switch').bootstrapSwitch('setState', $scope.boiler.status == "on" );
+//
+//            $scope.$apply();
+//        });
+//
+//        $( '.switch' ).on( 'switch-change', function ( e, data ) {
+//            var $el = $( data.el )
+//                , value = data.value;
+//
+//            var changeTo    = value ? 'on' : 'off';
+//
+//            $scope.boiler.status = changeTo;
+//            socket.emit( 'status', changeTo );
+//
+//        });
 
         $timeout(function(){
-            $scope.temperature = 30;
+            $scope.boiler.data = 55;
         },2000);
 
 
-        $scope.getTempClass = function(temperature){
-            if (temperature >= 0 && temperature < 25){
-                return "green";
+        $scope.getTempClass = function(){
+            if ( $scope.boiler.data >= 20 && $scope.boiler.data < 45 ){
+                return "blue";
             } else {
                 return "red";
+            }
+        }
+
+        $scope.isOn = function(){
+            if ( $scope.boiler.status == "on" ){
+                return "btn-danger";
+
+            } else {
+                return "";
+            }
+        }
+
+        $scope.isOff = function(){
+            if ( $scope.boiler.status == "off" ){
+                return "btn-primary";
+
+            } else {
+                return "";
             }
         }
 
