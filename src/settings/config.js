62c9e2568c2c815ea9079e74480206dca8aeca20
guyklainer <guyklainer@gmail.com>
2014-06-14 22:53:02 +0300

    fix

--- src/settings/config.js
+++ src/settings/config.js
@@ -21,12 +21,12 @@ module.exports = function(app, express, env) {
 // -- Global settings
 module.exports.settings = {
     'siteName'      : 'Goaa',
-    'sessionSecret' : 'SDFsdf34dfffsdfFSfASDasd311ksevvf',
+    'sessionSecret' : '',
     'uri'           : 'http://localhost', // Without trailing /
-    'productionURI' : 'ec2-54-246-47-98.eu-west-1.compute.amazonaws.com',
+    'productionURI' : '',
     'port'          : process.env.PORT || 3000,
     'debug'         : 0,
     'profile'       : 0,
-    'db'            : { main: 'mongodb://goaa:goaa@dharma.mongohq.com:10039/Goaa', fallback: 'mongodb://localhost/goaa' },
+    'db'            : { main: '', fallback: 'mongodb://localhost/goaa' },
     'defaultAvatar' : '/img/logo.png'
 }
